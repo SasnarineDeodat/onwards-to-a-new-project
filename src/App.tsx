91c36b0afbe7751e89095ee7604edfef74013d09
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-28 14:14:58 -0500

    Sharing Logic with "unknown" & Type Casting

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import Button from "./components/Button";
 import Input from "./components/Input";
 
 export default function App() {
+  function handleSave(data: unknown) {
+    const extractedData = data as {
+      name: string;
+      age: string;
+    };
+    console.log(data);
+  }
   return (
     <main>
-      <Form>
+      <Form onSave={handleSave}>
         <Input type="text" label="Name" id="name" />
         <Input type="number" label="Age" id="age" />
         <p>
