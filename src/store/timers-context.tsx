57a5ac55feef10832deb62862abf96f5c49fd7cf
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-28 16:03:10 -0500

    Accessing Context Type-Safe With A Custom Hook

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, createContext } from "react";
+import { type ReactNode, createContext, useContext } from "react";
 
 type Timer = {
   name: string;
@@ -16,7 +16,17 @@ type TimersContextValue = TimersState & {
   stopTimers: () => void;
 };
 
-const TimersContext = createContext<TimersContextValue | null>(null);
+export const TimersContext = createContext<TimersContextValue | null>(null);
+
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+
+  if (timersCtx === null) {
+    throw new Error("TimersContext is null - that should not be the case!");
+  }
+
+  return timersCtx;
+}
 
 type TimersContextProviderProps = {
   children: ReactNode;
@@ -27,7 +37,7 @@ export default function TimersContextProvider({
 }: TimersContextProviderProps) {
   const ctx: TimersContextValue = {
     timers: [],
-    isRunning: false,
+    isRunning: true,
     addTimer: (timerData) => {},
     startTimers: () => {},
     stopTimers: () => {},
